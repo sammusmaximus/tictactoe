1e693646aef2fe45fda9f45bf5ada84e04099e1a
Sam Mares <sam@Sams-MacBook-Air.local>
2023-05-31 11:17:00 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import mao from './assets/mao.jpeg';
+import commi from './assets/commi.png';
+import starling from './assets/starling.jpeg';
+import sung from './assets/sung.jpeg';
+import jongun from './assets/jongun.jpeg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="Comms">
+        <img src={commi} className='App-comms' alt="Commi symbol" />
+      </div>
+      <div className="Leaders">
+        <div className="Starling">
+          <a target="_blank" href="https://en.wikipedia.org/wiki/Joseph_Stalin">
+            <img src={starling} className="App-starling" alt="Joesph Starling" /></a>
+        </div>
+        <a target="_blank" href="https://en.wikipedia.org/wiki/Mao_Zedong">
+          <img src={mao} className="App-mao" alt="Chairman Mao" /></a>
+        <div className="Sung">
+          <a target="_blank" href="https://en.wikipedia.org/wiki/Kim_Il_Sung">
+            <img src={sung} className="App-sung" alt="Kim Il Sung" /></a>
+        </div>
+      </div>
+      <div className="Mao-Text">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Comrade, <code>Chairman Mao</code> looks to woooow
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=U06jlgpMtQs"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Come This Way Comrade
         </a>
-      </header>
-    </div>
+      </div>
+    </div >
   );
 }
 
